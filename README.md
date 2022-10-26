b1de07c0ed2ec181ef6a8b80cefa6629349ad441
Yuliya Kulpina <iyuliyaraspopina@gmail.com>
2022-10-26 13:34:11 +0800

    Commit two

---
+++ README.md
@@ -0,0 +1,1 @@
+[![Build status](https://ci.appveyor.com/api/projects/status/w0ndfx4nj276h51h?svg=true)](https://ci.appveyor.com/project/JuliaSiberia/delivery-card)
--- src/test/java/deliveryCardTest.java
+++ src/test/java/deliveryCardTest.java
@@ -1,2 +1,36 @@
+import com.codeborne.selenide.Condition;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+import static com.codeborne.selenide.Condition.exactText;
+import static com.codeborne.selenide.Selenide.*;
+
 public class deliveryCardTest {
+
+    String generateData(int days) {
+        return LocalDate.now().plusDays(days).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+    }
+
+    @Test
+    void enteringValidValues() {
+        String data = generateData(3);
+        open("http://localhost:9999");
+        $("[data-test-id=city] input").setValue("Иркутск");
+        $("[data-test-id=date] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.DELETE);
+        $("[data-test-id=date] input").sendKeys(data);
+        $("[data-test-id=name] input").setValue("Кульпина Юлия");
+        $("[data-test-id=phone] input").setValue("+98765432109");
+        $("[data-test-id=agreement").click();
+        $$("[type=button").find(exactText("Забронировать")).click();
+        $(".notification__content")
+                .shouldBe(Condition.visible, Duration.ofSeconds(15))
+                .shouldHave(exactText("Встреча успешно забронирована на " + data));
+
+
+    }
 }
